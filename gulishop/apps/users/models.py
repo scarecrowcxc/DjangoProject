5c2ffcf226a265dd9b9d826c46b6237ca31417d0
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-01-15 18:10:23 +0800

    create class UserProfile

--- gulishop/apps/users/models.py
+++ gulishop/apps/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 # Create your models here.
+
+
+
+class UserProfile(AbstractUser):
+    name = models.CharField(max_length=50, verbose_name='用户姓名', null=True, blank=True)
+    birthday = models.DateField(verbose_name='用户生日', null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=(('male', '男'), ('female', '女')))
+    mobile = models.CharField(max_length=11, verbose_name='用户手机', null=True, blank=True)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
